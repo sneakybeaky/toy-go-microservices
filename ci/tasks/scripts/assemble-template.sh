e0d9e82b72a169062011adbd9bf0c6dff2de0cbb
Jon Barber <jbarber@thoughtworks.com>
2017-12-21 15:27:05 +0000

    use ash shell

--- ci/tasks/scripts/assemble-template.sh
+++ ci/tasks/scripts/assemble-template.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/ash
 
-set -e -x
+#set -e -x
 
 find .
 
-VERSION=$(<version)
+#VERSION=$(<version)
 
-sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
+#sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
