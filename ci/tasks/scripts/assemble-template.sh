fd603391935ca7c838e70f629785430302c9096a
Jon Barber <jbarber@thoughtworks.com>
2017-12-21 15:39:53 +0000

    fixed path separator

--- ci/tasks/scripts/assemble-template.sh
+++ ci/tasks/scripts/assemble-template.sh
@@ -2,6 +2,6 @@
 
 set -e -x
 
-VERSION=`cat version\version`
+VERSION=`cat version/version`
 
 sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
