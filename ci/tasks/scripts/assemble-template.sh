8da39722bf324f2ce35a3696d4adc6880c815328
Jon Barber <jbarber@thoughtworks.com>
2017-12-21 15:10:33 +0000

    Changed path to version

--- ci/tasks/scripts/assemble-template.sh
+++ ci/tasks/scripts/assemble-template.sh
@@ -2,6 +2,6 @@
 
 set -e -x
 
-VERSION=$(<version/version)
+VERSION=$(<version)
 
 sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > k8s-template/deploy.yaml
