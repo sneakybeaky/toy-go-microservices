8e30ee7e9d69fd18754fa6b1e4f9f8e64f64fe0c
Jon Barber <jbarber@thoughtworks.com>
2017-12-21 15:36:10 +0000

    removed find

--- ci/tasks/scripts/assemble-template.sh
+++ ci/tasks/scripts/assemble-template.sh
@@ -2,9 +2,6 @@
 
 set -e -x
 
-find .
-
 VERSION=`cat version\version`
-
 
 sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
