4a26f8c6724633828dc6c4471f1ae9ba83cec2b6
Jon Barber <jbarber@thoughtworks.com>
2017-12-21 15:32:56 +0000

    Added back functionality

--- ci/tasks/scripts/assemble-template.sh
+++ ci/tasks/scripts/assemble-template.sh
@@ -1,9 +1,10 @@
 #!/bin/ash
 
-#set -e -x
+set -e -x
 
 find .
 
-#VERSION=$(<version)
+VERSION=`cat version\version`
 
-#sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
+
+sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
