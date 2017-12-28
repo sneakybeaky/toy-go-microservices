003efdff057a5d22f978141c9b2b49bb31b61894
Jon Barber <jon.barber@acm.org>
2017-12-28 14:05:36 +0000

    Removed everything bar version file

--- ci/tasks/scripts/assemble-k8s-template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-VERSION=$(<version/version)
-
-sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/k8s/deployment-template.yaml > k8s-template/deploy.yaml
