f45b4af799a99839f6f0602da271ee7eac608477
Jon Barber <jon.barber@acm.org>
2017-12-28 14:56:26 +0000

    Fixed path issues in Dockerfile

--- greet/ci/tasks/scripts/assemble-template.sh
+++ greet/ci/tasks/scripts/assemble-template.sh
@@ -4,4 +4,4 @@ set -e -x
 
 VERSION=`cat version/version`
 
-sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' greet-service/greet/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
+sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
