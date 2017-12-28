bad79a6b5c6b6d265b9c6f7fe22da42d446377a0
Jon Barber <jon.barber@acm.org>
2017-12-28 15:26:46 +0000

    Rename the repo resource

--- greet/ci/tasks/scripts/assemble-template.sh
+++ greet/ci/tasks/scripts/assemble-template.sh
@@ -4,4 +4,4 @@ set -e -x
 
 VERSION=`cat version/version`
 
-sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' service/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
+sed -r 's/^(.*image:.*)(test-front-service:.*$)/\1test-front-service:'"$VERSION"'/' source/greet/k8s/deployment-template.yaml > kubernetes-template/deploy.yaml
